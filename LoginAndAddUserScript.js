ac8503201bc9cc5b9765e166f2e1717557c8368d
CouldntThink225 <toughguy3245@gmail.com>
2024-09-28 22:16:32 -0500

    Update LoginAndAddUserScript.js
    Changed so the module error hopefully is resolved

--- LoginAndAddUserScript.js
+++ LoginAndAddUserScript.js
@@ -1,5 +1,5 @@
   // Initialize Supabase client
-  import { createClient } from '@supabase/supabase-js'
+  import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
   const supabaseUrl = 'https://fvypinxntxcpebvrrqpv.supabase.co';
   const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ2eXBpbnhudHhjcGVidnJycXB2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjczMTAyMDksImV4cCI6MjA0Mjg4NjIwOX0.Njr9v6k_QjA4ocszgB6SaPBauKvA4jNQSUj1cdOXCDg';
   const supabase = supabase.createClient(supabaseUrl, supabaseKey);
