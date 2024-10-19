121d8397d71c45828ca3ff204671ec01ad024bff
CouldntThink225 <toughguy3245@gmail.com>
2024-10-19 12:53:26 -0500

    Update Database.sql
    Created a database for emails imma see if this is better than supabase for now

--- Database.sql
+++ Database.sql
@@ -1,1 +1,6 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,       -- Automatically incrementing ID for each user
+    email VARCHAR(255) NOT NULL, -- User's email address
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP -- Timestamp of registration
+);
 
