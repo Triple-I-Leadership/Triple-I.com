26e822bfb717a8aa690ada410ebe61590e142165
CouldntThink225 <jakob.c.emerson@gmail.com>
2025-09-27 21:55:34 -0500

    Making a new branch lol

--- supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-
-const supabase = createClient(
-  'https://fvypinxntxcpebvrrqpv.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ2eXBpbnhudHhjcGVidnJycXB2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjczMTAyMDksImV4cCI6MjA0Mjg4NjIwOX0.Njr9v6k_QjA4ocszgB6SaPBauKvA4jNQSUj1cdOXCDg'
-);
-
-export default supabase;
